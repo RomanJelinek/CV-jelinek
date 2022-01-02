ae18de068b88d68edbec7be25a3f56be4d7ba5dc
RomanJelinek <roman.jelinek91@gmail.com>
2022-01-02 12:58:25 +0100

    test

--- src/navbar/Navmenu.js
+++ src/navbar/Navmenu.js
@@ -3,15 +3,7 @@ import React from 'react';
 const Navmenu = () => {
     return (
    <>
-   <Nav>
-     <NavLink to="/">
-       <h1>Home</h1>
-     </NavLink>
-     <Bars/>
-     <NavMenu>
-       <NavLink to="/timeline" activeStyle></NavLink>
-     </NavMenu>
-   </Nav>
+  
    </>
     );
 };
